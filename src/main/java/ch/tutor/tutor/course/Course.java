92c782675a2a02380cd0db300a9435aef8b9dd1a
Wiz <wizball6502@gmail.com>
2020-06-23 16:09:09 +0200

    Added getCourses Endpoint and Course Entity

---
+++ src/main/java/ch/tutor/tutor/course/Course.java
@@ -0,0 +1,25 @@
+package ch.tutor.tutor.course;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "course")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String course;
+
+    @Column
+    private String description;
+
+    @Column
+    private String author;
+
+}
